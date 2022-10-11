28d658bd3ee21203beb0d4d5851f78919d7793dd
brpgrrtygng <74527999+svehlap7@users.noreply.github.com>
2022-10-11 10:31:41 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace VICE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(238, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 41);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(238, 95);
+            this.listBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Crimson;
+            this.ClientSize = new System.Drawing.Size(262, 152);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "VICE";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VICE
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int pocet = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+            ColorDialog barva = new ColorDialog();
+        }
+        private void CheckEnter(object sender, System.Windows.Forms.KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            {
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (pocet < 10)
+            {
+
+                Button button1 = new Button(); 
+                button1.DialogResult = DialogResult.OK;
+                button1.Location = new Point(130, 189);
+                button1.Size = new Size(75, 23);
+                button1.Text = "ULOZIT";
+
+
+                Button button2 = new Button();
+                button2.DialogResult = DialogResult.Cancel;
+                button2.Location = new Point(130, 130);
+                button2.Size = new Size(75, 23);
+                button2.Text = "ZRUSIT";
+
+
+                TextBox textBox1 = new TextBox();
+                textBox1.Clear();
+
+                this.AcceptButton = button1;
+                textBox1.Location = new Point(49, 40);
+                textBox1.Size = new Size(188, 22);
+
+
+                Form form2 = new Form();
+                form2.Location = new Point(300, 600);
+                form2.Controls.Add(textBox1);
+                form2.Controls.Add(button2);
+                form2.Controls.Add(button1);
+                form2.Text = "MOZNOSTI";
+
+                if (form2.ShowDialog() == DialogResult.OK)
+                {
+                    listBox1.Items.Add(textBox1.Text);
+                    pocet++;
+                }
+
+            }
+        }
+
+    }
+}
